4e8fb0fe5e95c9966b182e041d4202c58268df0f
John Ralston <john.e.ralston@gmail.com>
2024-02-13 23:46:40 +0000

    add internal state and props eg.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import "./style.css";
 import { MyComp } from "./components/mycomp";
 import { mount } from "./framework/tag";
 
-const comp = MyComp();
-const comp2 = MyComp();
+const comp = MyComp({ name: "ONE" });
+const comp2 = MyComp({ name: "TWO" });
 mount("#app", `<div>${comp}${comp2}</div>`);
